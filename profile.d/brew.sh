05ed63bfadfdeeb9b0d8945b74cc3b294d11c696
oqkr <oqkr@protonmail.com>
2019-02-12 11:40:58 -0600

    Disable shellcheck SC1090
    https://github.com/koalaman/shellcheck/wiki/SC1090

--- profile.d/brew.sh
+++ profile.d/brew.sh
@@ -9,6 +9,8 @@ command -v brew >/dev/null || return
 
 # Add brew command completions for Bash. (Equivalent Zsh configuration for this
 # lives in ~/.zshrc since it needs to happen before loading ~/.oh-my-zsh.)
+#
+# shellcheck disable=1090
 if [[ -n "${BASH}" && -n "${BASH_VERSION}" ]]; then
   for completion in "$(brew --prefix)"/etc/bash_completion.d/*; do
     [[ -f "${completion}" ]] && source "${completion}"
