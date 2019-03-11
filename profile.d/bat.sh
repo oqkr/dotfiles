9181695d1a4c5bae632cdf1408e43c9c326d5afa
oqkr <oqkr@protonmail.com>
2019-03-11 04:03:53 -0500

    Add alias for cat -> bat

---
+++ profile.d/bat.sh
@@ -0,0 +1,10 @@
+# · · · · · · · · · · · · · · · · · · · · · · · · · · · · · · · · · · · · · · ·
+# Add settings for bat (https://github.com/sharkdp/bat).
+#
+# Author: oqkr@pm.me
+# Source: https://github.com/oqkr/dotfiles
+# · · · · · · · · · · · · · · · · · · · · · · · · · · · · · · · · · · · · · · ·
+
+command -v bat &>/dev/null || return
+
+alias cat='bat'
