15f7eb965b298d0dd392ed908a219fe0e899f7e1
oqkr <oqkr@protonmail.com>
2019-03-22 02:28:21 -0500

    Fix bug where java path set incorrectly

--- profile.d/java.sh
+++ profile.d/java.sh
@@ -8,6 +8,6 @@
 # Set JAVA_HOME for macOS systems.
 if [[ "${DOTFILES_OS_TYPE}" == "darwin" ]]; then
   if command -v /usr/libexec/java_home >/dev/null; then
-    export JAVA_HOME="/usr/libexec/java_home"
+    export JAVA_HOME="$(/usr/libexec/java_home)"
   fi
 fi
